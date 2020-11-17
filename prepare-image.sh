2304922a172df00bbd9b5a76b5d5bcca6f34e938
Peter Mansour <pmansour112@gmail.com>
2020-11-16 22:06:27 -0800

    comment out parts of prepare-image, fix WIFI config

--- prepare-image.sh
+++ prepare-image.sh
@@ -2,24 +2,28 @@
 # Run this to flash the SD card image.
 # Note: this doesn't work in WSL2 due to lack of USB device passthrough.
 
-RASPBIAN_DOWNLOAD_URL='https://downloads.raspberrypi.org/raspios_full_armhf/images/raspios_full_armhf-2020-08-24/2020-08-20-raspios-buster-armhf-full.zip'
+# RASPBIAN_DOWNLOAD_URL='https://downloads.raspberrypi.org/raspios_full_armhf/images/raspios_full_armhf-2020-08-24/2020-08-20-raspios-buster-armhf-full.zip'
 
-echo "Downloading OS image.."
-wget -nc -O "raspbian.zip" "$RASBIAN_DOWNLOAD_URL"
+# echo "Downloading OS image.."
+# wget -nc -O "raspbian.zip" "$RASBIAN_DOWNLOAD_URL"
 
-# TODO: show options of connected file mounts and allow
-# user to choose one.
-DEVICE="/dev/sdX"
-echo "Writing image to SD card at '$DEVICE'.."
-unzip -p "raspbian.zip" | sudo dd of="$DEVICE" bs=4M conv=fsync status=progress
+# # TODO: show options of connected file mounts and allow
+# # user to choose one.
+# DEVICE="/dev/sdX"
+# echo "Writing image to SD card at '$DEVICE'.."
+# unzip -p "raspbian.zip" | sudo dd of="$DEVICE" bs=4M conv=fsync status=progress
 
 # Configure WiFi.
-SD_MOUNT_POINT='/mnt/foo'
-BOOT_DIR="$SD_MOUNT_POINT/boot"
+BOOT_DIR="/mnt/d"
 read -p "WiFi network name: " NETWORK_NAME
 read -p "WiFi network password: " -s NETWORK_PASS
+echo
 
-tee "$BOOT_DIR/wpa_supplicant.conf" >&2 < EOF
+cat >"$BOOT_DIR/wpa_supplicant.conf" <<EOF
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+country=US
+
 network={
     ssid="$NETWORK_NAME"
     psk="$NETWORK_PASS"
