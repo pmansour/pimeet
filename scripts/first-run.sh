123e024bc7249d2e287454437ca5e84424d398b1
Peter Mansour <pmansour112@gmail.com>
2021-09-18 14:06:53 -0700

    add script for dynamic initialization on first run

---
+++ scripts/first-run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Script needs to be run as root (e.g. with sudo).
+
+if [[ "`id -u`" -ne 0 ]]; then
+    echo "This csript must be run as root. Try running 'sudo $1'."
+    exit 1
+fi
+
+echo "Updating packages.."
+apt-get update && sudo apt-get upgrade --yes
+
+# Enable VNC server.
+echo
+echo "Installing and enabling VNC server.."
+apt-get install --yes --quiet realvnc-vnc-server
+systemctl enable vncserver-x11-serviced.service && \
+    systemctl start vncserver-x11-serviced.service
+
+# Configure autologin.
+echo
+echo "Configuring autologin.."
+apt-get install --yes --quiet lightdm
+# See https://github.com/RPi-Distro/raspi-config/blob/master/raspi-config#L1339
+systemctl set-default graphical.target
+ln -fs /lib/systemd/system/getty@.service /etc/systemd/system/getty.target.wants/getty@tty1.service
+cat > /etc/systemd/system/getty@tty1.service.d/autologin.conf << EOF
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin pi --noclear %I \$TERM
+EOF
+sed /etc/lightdm/lightdm.conf -i -e "s/^\(#\|\)autologin-user=.*/autologin-user=pi/"
+
+# Configure HDMI-CEC
+echo
+echo "Configuring HDMI-CEC"
+apt-get install --yes --quiet cec-utils
