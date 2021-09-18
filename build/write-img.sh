6e9a4715e29e9164955d450ab60d13ebd916c829
Peter Mansour <pmansour112@gmail.com>
2021-09-18 12:48:22 -0700

    update write-img script

--- build/write-img.sh
+++ build/write-img.sh
@@ -6,6 +6,17 @@
 IMG_FILE="$HOME/raspios-img/2021-05-07-raspios-buster-armhf-full.img"
 DEVICE='/dev/sda' # Find intended device (not partition) by running `lsblk`.
 
-ls "$DEVICE"?* | sudo xargs -I {} ! mountpoint -q {} || umount {}
+function confirm_device() {
+    sudo fdisk -l "$DEVICE"
+    echo
+    read -p "Confirm formatting device '$DEVICE'? [y/N]" -n 1 -r
+    echo
+    if [[ ! "$REPLY" =~ ^[Yy]$ ]]; then
+        echo "Aborted."
+        exit 1
+    fi
+}
 
+confirm_device
+ls "$DEVICE"?* | sudo xargs umount -q
 sudo dd bs=4M status=progress if="$IMG_FILE" of="$DEVICE"
