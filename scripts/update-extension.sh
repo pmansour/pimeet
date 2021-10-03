fffb9d41ab2c94000680e2c7527fd78e1b1bf723
Peter Mansour <pmansour112@gmail.com>
2021-10-03 13:55:09 -0700

    fixing update script to add chmod

--- scripts/update-extension.sh
+++ scripts/update-extension.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ "`id -u`" -ne 0 ]]; then
+    echo "This script must be run as root. Try running 'sudo $1'."
+    exit 1
+fi
+
 DOWNLOAD_URL='https://github.com/pmansour/minimeet/releases/download/stable/mv3.zip'
 TMP_ZIP_FILE=`mktemp`
 EXTENSION_SRC_DIR='/usr/local/minimeet'
@@ -22,6 +27,9 @@ rm "$TMP_ZIP_FILE"
 echo "Restoring credentials file.."
 mkdir -p "$EXTENSION_SRC_DIR/config"
 cp "$CREDS_BACKUP_FILE" "$EXTENSION_SRC_DIR/config/creds.js"
+
+echo "Updating permissions.."
+chmod -R a+rwx /usr/local/minimeet
 
 echo "Done."
 
