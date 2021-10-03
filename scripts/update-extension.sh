cd80e61604c6ea6014900d9dc0b61988f65ec632
Peter Mansour <pmansour112@gmail.com>
2021-10-03 11:40:24 -0700

    Add script to update the extension

---
+++ scripts/update-extension.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DOWNLOAD_URL='https://github.com/pmansour/minimeet/releases/download/stable/mv3.zip'
+TMP_ZIP_FILE=`mktemp`
+EXTENSION_SRC_DIR='/usr/local/minimeet'
+
+# Backup credentials file.
+echo "Backing up credentials file.."
+CREDS_BACKUP_FILE=`mktemp`
+cp "$EXTENSION_SRC_DIR/config/creds.js" "$CREDS_BACKUP_FILE"
+
+# Download and unzip new extension
+echo "Downloading latest extension.."
+wget -q --show-progress "$DOWNLOAD_URL" -O "$TMP_ZIP_FILE"
+echo "Deleting old extension.."
+rm -rf "$EXTENSION_SRC_DIR"
+echo "Extracting archive to '$EXTENSION_SRC_DIR'.."
+mkdir -p "$EXTENSION_SRC_DIR"
+unzip -d "$EXTENSION_SRC_DIR" "$TMP_ZIP_FILE"
+rm "$TMP_ZIP_FILE"
+
+echo "Restoring credentials file.."
+mkdir -p "$EXTENSION_SRC_DIR/config"
+cp "$CREDS_BACKUP_FILE" "$EXTENSION_SRC_DIR/config/creds.js"
+
+echo "Done."
+
